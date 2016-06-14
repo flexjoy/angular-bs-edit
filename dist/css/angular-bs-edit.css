99bd4e798d8975e67da2c822948ffbc878c7871d
Sergey Cherepanov <flexjoy@ya.ru>
2016-06-14 10:33:14 +0300

    Add angular-bs-edit.css file

---
+++ dist/css/angular-bs-edit.css
@@ -0,0 +1,6 @@
+/**
+ * Angular directives for inline editing v0.0.1 (https://github.com/flexjoy/angular-bs-edit)
+ *
+ * Copyright 2016 Sergey Cherepanov
+ * Licensed under the MIT license (https://opensource.org/licenses/mit-license.php)
+ */
