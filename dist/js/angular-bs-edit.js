bc0cf7af948514628fd9059b8c6b7b4c123cf1a5
Sergey Cherepanov <flexjoy@ya.ru>
2016-06-14 15:11:08 +0300

    Add bseSelect directive

--- dist/js/angular-bs-edit.js
+++ dist/js/angular-bs-edit.js
@@ -40,7 +40,6 @@ app.directive('bseText', function() {
 			});
 
 			inputElement.bind('keydown', function (event) {
-
 				if(event.which === 13) {
 					this.blur();
 					event.preventDefault();
@@ -48,6 +47,62 @@ app.directive('bseText', function() {
 
 				if(event.which === 27) {
 					scope.newValue = scope.value;
+					this.blur();
+					event.preventDefault();
+				}
+			});
+		}
+	};
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// bseSelect directive
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+app.directive('bseSelect', function() {
+	var showDropdown = function (element) {
+		var event = document.createEvent('MouseEvents');
+		event.initMouseEvent("mousedown", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+		setTimeout(function(){
+			element.dispatchEvent(event);
+		}, 1);
+	};
+	return {
+		restrict: 'A',
+		scope: {
+			value: '=bseSelect',
+			onshow: '&',
+			empty: '=?'
+		},
+		template: '<select ng-model="value" class="form-control" ng-options="v.name for v in values track by v.id"></select><span class="form-control" ng-class="{\'bse-empty\' : !value}" ng-bind="value.name || empty"></span>',
+		link: function(scope, element) {
+			
+			scope.empty = scope.empty ? scope.empty : 'empty';
+			element.addClass('edit-in-place');
+			var inputElement = angular.element(element.children()[0]);
+			
+			element.bind('click', function () {
+				scope.onshow().then(
+					function (data){
+						scope.values = data;
+						element.addClass('active');
+						inputElement[0].focus();
+						showDropdown(inputElement[0]);
+					}
+				);
+			});
+			
+			inputElement.bind('blur', function() {
+				scope.$apply();
+				element.removeClass('active');
+			});
+			
+			inputElement.bind('change', function(event) {
+				this.blur();
+				event.preventDefault();
+			});
+			
+			inputElement.bind('keydown', function (event) {
+				if(event.which === 27) {
 					this.blur();
 					event.preventDefault();
 				}
--- test/js/script.js
+++ test/js/script.js
@@ -1,12 +1,30 @@
 'use strict';
 
-app.controller('Ctrl', function($scope) {
+app.controller('Ctrl', function($scope, $q) {
 	var data = {
-		text: 'This is a sample text'
+		text: 'This is a sample text',
+		select: { id: 3, name: 'London'}
 	};
+
+	var list = [
+		{id: 1, name: 'Moscow'},
+		{id: 2, name: 'New-York'},
+		{id: 3, name: 'London'},
+		{id: 4, name: 'Paris'}
+	];
 	
 	$scope.clear = function () {
 		$scope.data.text = '';
+		$scope.data.select = '';
+	};
+
+	$scope.getList = function() {
+		console.log('called getList');
+		var d = $q.defer();
+		setTimeout(function() {
+			d.resolve(list)
+		}, 100);
+		return d.promise;
 	};
 	
 	$scope.fill = function () {
